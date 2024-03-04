d9ec4ed4598840ff0d17f56fc3535510fde2c0c1
gernest <geofreyernest@live.com>
2024-03-04 22:23:33 +0300

    use monokai light

--- docs.go
+++ docs.go
@@ -117,7 +117,7 @@ func renderPage(w io.Writer, id func() int, seen map[string]struct{}, text []byt
 		Base: blackfriday.NewHTMLRenderer(blackfriday.HTMLRendererParameters{
 			Flags: blackfriday.CommonHTMLFlags,
 		}),
-		Style:      styles.Get("monokai"),
+		Style:      styles.Get("monokailight"),
 		Autodetect: true,
 	}
 	r.Formatter = bhtml.New(r.ChromaOptions...)
